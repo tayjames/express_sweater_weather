d3db833f953727163cf75faca7570c4594130177
Tay <tay.james.d@gmail.com>
2019-10-07 07:13:33 -0600

    Defines routes for favorite

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sessionsRouter = require('./routes/sessions');
 var forecastRouter = require('./routes/forecast');
+var favoriteRouter = require('./routes/favorite')
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/sessions', sessionsRouter)
 app.use('/api/v1/forecast', forecastRouter)
+app.use('/api/v1/favorite', favoriteRouter)
 
 module.exports = app;
